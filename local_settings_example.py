a6c0acfcb6080090a2f825537f072fa0c120ccc3
Brian Enigma <brian@netninja.com>
2017-04-29 10:00:32 -0700

    Fix inline doc

--- local_settings_example.py
+++ local_settings_example.py
@@ -9,6 +9,6 @@ MY_ACCESS_TOKEN_KEY = 'Your Twitter API Access Token Key'
 MY_ACCESS_TOKEN_SECRET = 'Your Access Token Secret'
 BING_API_KEY = "Your Bing API key"
 
-ODDS = 3 #How often do you want this to run? 1/8 times?
+ODDS = 3 #How often do you want this to run? 1/3 times?
 DEBUG = True #Set this to False to start Tweeting live
 
